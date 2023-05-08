e7635f599b77c2c731a0a4193fe3675b213a8c84
AnastasiaTelushkina <telushkinanasta@gmail.com>
2023-05-08 23:34:37 +0300

    feat: made 5th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,18 @@
 const fs = require('fs'); 
-const initPath = "04-copy-directory/files";
-const copyPath = "04-copy-directory/files-copy";
- 
+
+function copyDir(initPath, copyPath){
 fs.mkdir(copyPath, { recursive: true }, (err) => { if (err) throw err;}); 
  
 fs.readdir(initPath,{withFileTypes: true}, (err, files) => {  
     if (err) throw err;
     files.forEach((file) =>{ 
-        if(file.isFile()){   
-            fs.copyFile(copyPath + "/"+ file.name, copyPath + "/"+ file.name, (err) => {
+        if(file.isFile()){    
+            fs.copyFile(initPath + "/"+ file.name, copyPath + "/"+ file.name, (err) => {
                 if (err)  console.log("Error Found:", err); 
                 else console.log(initPath + "/"+ file.name + " copied"); 
             });
         }
     });
  });
+}
+copyDir("04-copy-directory/files", "04-copy-directory/files-copy");
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,15 +1,16 @@
-const fs = require('fs'); 
+const fs = require("fs"); 
+const path = require('node:path'); 
 const folderPath = "05-merge-styles/styles"; 
   
  
 fs.readdir(folderPath,{withFileTypes: true}, (err, files) => {  
     if (err) throw err;
-    files.forEach((file) =>{ 
-        if(file.isFile()){   
-            fs.copyFile(copyPath + "/"+ file.name, copyPath + "/"+ file.name, (err) => {
-                if (err)  console.log("Error Found:", err); 
-                else console.log(initPath + "/"+ file.name + " copied"); 
-            });
+    const output = fs.createWriteStream("05-merge-styles/bundle.css");
+    files.forEach((file) =>{  
+        if(file.isFile() && path.parse(file.name).ext === ".css"){  
+            console.log(file.name);
+            const input = fs.createReadStream(folderPath + "/" + file.name, "utf-8");  
+            input.pipe(output);
         }
     });
  });
