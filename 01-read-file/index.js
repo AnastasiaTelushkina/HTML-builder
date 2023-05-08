c2df185946f386e327480e85eab934c6826fe119
AnastasiaTelushkina <telushkinanasta@gmail.com>
2023-05-08 21:12:22 +0300

    feat: made 2nd task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,4 @@
-const fs = require("fs");  
-//fs.readFile("01-read-file\\text.txt", "utf8",  (error,data) => {  if(error) throw error;  console.log(data);});
+const fs = require("fs");   
 var stream = new fs.ReadStream("01-read-file\\text.txt", {encoding: "UTF-8"});
 
 stream.on('readable', function(){
---
+++ 02-write-file/index.js
@@ -0,0 +1,13 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const output = fs.createWriteStream('02-write-file\\result.txt');
+
+stdout.write("Hello! Enter your text:\n")
+stdin.on("data", data => {  
+  const dataStringified = data.toString("utf8"); 
+  if(dataStringified != "exit\r\n")  output.write(dataStringified); 
+  else process.exit();  
+}); 
+process.on('exit', () => stdout.write("Text saved"));
+ 
+ 
