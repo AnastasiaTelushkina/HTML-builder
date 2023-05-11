e1181fded7253aaa3f73dc5853f96986ca651eb3
AnastasiaTelushkina <telushkinanasta@gmail.com>
2023-05-11 15:25:44 +0300

    fix: edit 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,34 @@
-const fs = require('fs'); 
+const fs = require('fs');
+const path = require('path');
+const filesFolder = path.join(__dirname, 'files');
+const copiedFolder = path.join(__dirname, 'files-copy');
 
-function copyDir(initPath, copyPath){
-  fs.mkdir(copyPath, { recursive: true }, (err) => { if (err) throw err;}); 
- 
-  fs.readdir(initPath,{withFileTypes: true}, (err, files) => {  
-    if (err) throw err;
-    files.forEach((file) =>{ 
-      if(file.isFile()){    
-        fs.copyFile(initPath + '/'+ file.name, copyPath + '/'+ file.name, (err) => {
-          if (err)  console.log('Error Found:', err); 
-          else console.log(initPath + '/'+ file.name + ' copied'); 
+function copyDir(filesFolder, copiedFolder) {
+  fs.rm(path.join(copiedFolder), {recursive: true, force: true}, err => {
+    if (err) console.log(err);
+    fs.mkdir(path.join(copiedFolder), {recursive: true}, err => {
+      if (err) return console.error(err);
+    });
+    fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        files.forEach(file => {
+          const dir = path.join(filesFolder, file.name);
+          const copied = path.join(copiedFolder, file.name);
+          if (file.isFile()) {
+            fs.copyFile(dir, copied, (err) => {
+              if (err) {
+                console.log('Error:', err);
+              }  
+            });
+          } else if (file.isDirectory()) {
+            copyDir(dir, copied); 
+          }
         });
       }
     });
   });
 }
-copyDir('04-copy-directory/files', '04-copy-directory/files-copy');
+
+copyDir(filesFolder, copiedFolder);
