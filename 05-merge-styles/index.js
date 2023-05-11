7343eda894b2465f836a86afb0b7e1b926cc31b1
AnastasiaTelushkina <telushkinanasta@gmail.com>
2023-05-11 22:29:44 +0300

    fix: edit 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,7 +5,7 @@ const folderPath = '05-merge-styles/styles';
  
 fs.readdir(folderPath,{withFileTypes: true}, (err, files) => {  
   if (err) throw err;
-  const output = fs.createWriteStream('05-merge-styles/bundle.css');
+  const output = fs.createWriteStream('05-merge-styles/project-dist/bundle.css');
   files.forEach((file) =>{  
     if(file.isFile() && path.extname(file.name) === '.css'){   
       const input = fs.createReadStream(folderPath + '/' + file.name, 'utf-8');  
